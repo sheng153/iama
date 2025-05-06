9bc7a0306b093fc0a41d0ecd408155f26858ddc6
sheng153 <nicolas.sabio@gmail.com>
2025-05-06 11:35:16 -0300

    docker y __name__=='__main__'

--- model/training.py
+++ model/training.py
@@ -8,63 +8,69 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 
-#lemmatization utiliza reglas del lenguaje para obtener la base o raiz de una palabra
-lemmatizer =WordNetLemmatizer()
-#importo los intents
-intents = json.loads(open('intents.json').read())
+IGNORE_LETTERS = ['?','!','.',',']
 
-nltk.download('punkt', halt_on_error=False)
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+def initialize_nltk():
+    nltk.download('punkt', halt_on_error=False)
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
 
-#creo las listas
-words=[]
-classes = []
-ignore_letters= ['?','!','.',',']
+def initialize_intents():
+    return (json.loads(open("intents.json").read()), WordNetLemmatizer())
 
-documents = [
-    (
-        [lemmatizer.lemmatize(w.lower()) 
-         for w in nltk.word_tokenize(pattern)
-         if w not in ignore_letters],
-        intent['tag']
-    )
-    for intent in intents['intents']
-    for pattern in intent['patterns']
-]
+def set_documents(intents, lemmatizer) -> List[Tuple[List[str], str]]:
+    return [
+        (
+            [lemmatizer.lemmatize(w.lower()) 
+                for w in nltk.word_tokenize(pattern)
+                if w not in IGNORE_LETTERS],
+            intent['tag']
+        )
+        for intent in intents['intents']
+        for pattern in intent['patterns']
+    ]
 
-words   = sorted({ w for doc, _ in documents for w in doc }) 
-classes = sorted({ tag for _, tag in documents })
+def set_BoW(docs):
+    words   = sorted({ w for doc, _ in documents for w in doc }) 
+    classes = sorted({ tag for _, tag in documents })
 
-pickle.dump(words,   open('words.pkl','wb'))
-pickle.dump(classes, open('classes.pkl','wb'))
+    pickle.dump(words,   open('words.pkl','wb'))
+    pickle.dump(classes, open('classes.pkl','wb'))
 
-training = [
+    return [
     (
         [1 if w in doc else 0 for w in words],
         [1 if c == tag else 0 for c in classes]
     )
     for doc, tag in documents
-]
+    ]
 
-#ahora divido en 2 variables
-train_x, train_y = map(list, zip(*training))
+def instantiate_model(train_x, train_y):
+    model = Sequential()
 
-#creo la red neuronal secuencial
-model = Sequential()
-#primera capa
-model.add(Dense(128, input_shape=(len(train_x[0]),), name="inp_layer", activation='relu'))
-model.add(Dropout(0.5, name="hidden_layer1"))
-#segunda capa
-model.add(Dense(64, name="hidden_layer2", activation='relu'))
-model.add(Dropout(0.5, name="hidden_layer3"))
-#capa de salida
-model.add(Dense(len(train_y[0]), name="output_layer", activation='softmax'))
+    model.add(Dense(128, input_shape=(len(train_x[0]),), name="inp_layer", activation='relu'))
+    model.add(Dropout(0.5, name="hidden_layer1"))
 
-#creo el optimizador y compilo
-sgd = SGD(learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+    model.add(Dense(64, name="hidden_layer2", activation='relu'))
+    model.add(Dropout(0.5, name="hidden_layer3"))
 
-#se entrena el modelo y lo guardo
-model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
-model.save("chatbot_model.h5")
+    model.add(Dense(len(train_y[0]), name="output_layer", activation='softmax'))
+
+    sgd = SGD(learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+    return model
+
+if __name__ == '__main__': 
+    initialize_nltk()
+
+    intents, lemmatizer = initialize_intents() 
+
+    documents = set_documents(intents, lemmatizer)
+
+    train_x, train_y = map(list, zip(*set_BoW(documents)))
+
+    model = instantiate_model(train_x, train_y)
+
+    model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
+    model.save("chatbot_model.h5")
